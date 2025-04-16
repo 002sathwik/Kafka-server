9562b98e289c5669721639d75350e620adf1759f
Sathwik <hsathwik002@gmail.com>
2025-04-16 21:00:45 +0530

    [fix];ui feath:landingpage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
-
-export default function Home() {
+import Navbar from "@/components/base/Navbar";
+import HeroSection from "@/components/base/HeroSection";
+import FeatureSection from "@/components/base/FeatureSection";
+import UserReviews from "@/components/base/UserReviews";
+import Footer from "@/components/base/Footer";
+// import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+// import { getServerSession } from "next-auth";
+export default async function LandingPage() {
+  // const session: CustomSession | null = await getServerSession(authOptions);
   return (
-    <div>
-      hi
+    <div className="min-h-screen flex flex-col ">
+      {/* Header */}
+      <Navbar  />
+      {/* Hero Section */}
+      <HeroSection />
+
+      {/* Features Section */}
+      <FeatureSection />
+
+      {/* User Reviews Section */}
+      <UserReviews />
+
+      {/* Footer */}
+      <Footer />
     </div>
   );
 }
