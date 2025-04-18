23ec0f1b1de1a1ae8ec1ff6ad9bfd753e3eb8389
Sathwik <hsathwik002@gmail.com>
2025-04-18 19:49:47 +0530

    [add]:login-ui component

---
+++ client/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,23 @@
+import { AuthOptions } from "next-auth";
+
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions: AuthOptions = {
+    pages: {
+        signIn: '/',
+
+    },
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID!,
+            clientSecret: process.env.GOOGLE_SECRET!,
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code"
+                }
+            }
+        }),
+
+    ]
+}
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,7 @@ import FeatureSection from "@/components/base/FeatureSection";
 import UserReviews from "@/components/base/UserReviews";
 import Footer from "@/components/base/Footer";
 // import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
-// import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 export default async function LandingPage() {
   // const session: CustomSession | null = await getServerSession(authOptions);
   return (
