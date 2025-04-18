52d6aa393a59997533dec4b2c16b5cb1213ec2e9
Sathwik <hsathwik002@gmail.com>
2025-04-18 20:04:06 +0530

    [add]:Next-Auth Google /session

--- client/src/app/api/auth/[...nextauth]/options.ts
+++ client/src/app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,63 @@
-import { AuthOptions } from "next-auth";
+import { AuthOptions, ISODateString } from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 import GoogleProvider from "next-auth/providers/google";
+
+
+
+
+export type CustomerSession = {
+    user?: CustomerUser,
+    expires: ISODateString
+}
+export type CustomerUser = {
+    id?: string | null,
+    name?: string | null,
+    email?: string | null,
+    image?: string | null,
+    provider?: string | null,
+    token?: string | null,
+}
+
 export const authOptions: AuthOptions = {
     pages: {
         signIn: '/',
+
+    },
+
+    callbacks: {
+        async signIn({
+             user, 
+             account 
+            }) {
+            console.log("The User", user)
+            console.log("The Account", account)
+            return true
+        },
+        // async redirect({ url, baseUrl }) {
+        //     return baseUrl
+        // },
+        async session({
+            session,
+            user,
+            token
+        }: {
+            session: CustomerSession,
+            user: CustomerUser,
+            token: JWT
+        }) {
+            session.user = token.user as CustomerUser
+            return session
+        },
+        async jwt({
+            token,
+            user
+        }) {
+            if (user) {
+                token.user = user
+            }
+            return token
+        }
 
     },
     providers: [
